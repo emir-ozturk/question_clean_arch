0b2cb65b8c6ade8d20c422f6d5abe3a9281e8a5f
emir1719 <emirzr56@gmail.com>
2025-07-05 14:33:56 +0300

    servis ve hata yönetimi yazıldı

---
+++ lib/core/error/dio_error_handler.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class DioErrorHandler {
+  static String handle(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+        return 'error.connection_timeout'.tr();
+      case DioExceptionType.sendTimeout:
+        return 'error.send_timeout'.tr();
+      case DioExceptionType.connectionError:
+        return 'error.connection_error'.tr();
+      case DioExceptionType.badResponse:
+        return 'error.type.${error.response?.statusCode ?? "404"}'.tr();
+      default:
+        return 'error.server_error'.tr();
+    }
+  }
+}
---
+++ lib/features/question_answer/data/data_source/remote/question_api_service.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:question_clean_arch/core/error/dio_error_handler.dart';
+import 'package:question_clean_arch/features/question_answer/data/models/category_model.dart';
+import 'package:question_clean_arch/features/question_answer/data/models/question_model.dart';
+
+class QuestionApiService {
+  final Dio _dio;
+
+  QuestionApiService([Dio? dio])
+    : _dio = dio ?? Dio(BaseOptions(baseUrl: 'https://soru.sozlerkosku.com/api'));
+
+  Future<List<QuestionModel>> getQuestions(int categoryId) async {
+    try {
+      final response = await _dio.get('/sorular/$categoryId');
+      return (response.data['sorular'] as List).map((e) => QuestionModel.fromJson(e)).toList();
+    } on DioException catch (e) {
+      throw Exception(DioErrorHandler.handle(e));
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<List<CategoryModel>> getCategories(String languageCode) async {
+    try {
+      final response = await _dio.get('/kategoriler', queryParameters: {'lang': languageCode});
+      return response.data.map((e) => CategoryModel.fromJson(e)).toList();
+    } on DioException catch (e) {
+      throw Exception(DioErrorHandler.handle(e));
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
